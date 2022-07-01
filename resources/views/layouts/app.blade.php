cd56bf697712f5e1311f2b368bc63d4ce18a0bed
Abdessamad <a.berahhou@edu.umi.ac.ma>
2022-07-01 09:41:56 +0000

    third commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
@@ -27,6 +28,7 @@
     <link rel="stylesheet" href="{{asset('icons-1.8.1/font/bootstrap-icons.css')}}">
     <link rel="stylesheet" href="{{asset('font-awesome/css/all.css')}}">
     <link rel="stylesheet" href="{{asset('pro/css/all.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div>
